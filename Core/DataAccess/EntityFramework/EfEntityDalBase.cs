f221bd4e4462c10ff2acf2eef06fe4ba38f8e8c2
Özge Odabaş <180315051@ogr.cbu.edu.tr>
2023-11-19 01:44:55 +0300

    generic Base class created for dal classes

---
+++ Core/DataAccess/EntityFramework/EfEntityDalBase.cs
@@ -0,0 +1,72 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfEntityDalBase<TEntity, TContext>: IEntityRepository<TEntity>
+        where TEntity : class,IEntity, new()
+        where TContext : DbContext, new()
+    {
+
+        public void Add(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                context.Set<TEntity>().Add(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (TContext context = new TContext())
+            {
+                var entity = context.Set<TEntity>().Find(id);
+                if (entity != null)
+                {
+                    context.Set<TEntity>().Remove(entity);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null ? context.Set<TEntity>().ToList() : context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(int id, TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var e = context.Set<TEntity>().Find(id);
+                if (e != null )
+                {
+                    context.Entry(e).State = EntityState.Detached;
+                    var updatedEntity = context.Entry(entity);
+                    updatedEntity.State = EntityState.Modified;
+                   
+                    context.SaveChanges();
+                }
+            }
+        }
+
+    }
+}
--- DataAccess/Concrete/EntityFramework/EfUserDal.cs
+++ DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -1,5 +1,5 @@
-﻿using DataAccess.Abstract;
-using Entities.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
@@ -11,57 +11,8 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfUserDal : IUserDal
+    public class EfUserDal : EfEntityDalBase<User,EatItContext>,IUserDal
     {
-        public void Add(User entity)
-        {
-            using (EatItContext context = new EatItContext())
-            {
-                context.Users.Add(entity);
-                context.SaveChanges();
-            }
-        }
-
-        public void Delete(int id)
-        {
-            using (EatItContext context = new EatItContext())
-            {
-                var entity = context.Users.FirstOrDefault(x => x.UserId == id);
-                if (entity != null)
-                {
-                    context.Users.Remove(entity);
-                    context.SaveChanges();
-                }
-            }
-        }
-
-        public User Get(Expression<Func<User, bool>> filter)
-        {
-            using (EatItContext context = new EatItContext())
-            {
-                return context.Users.SingleOrDefault(filter);
-            }
-        }
-
-        public List<User> GetAll(Expression<Func<User, bool>> filter = null)
-        {
-            using(EatItContext context = new EatItContext()){
-                 return filter==null ? context.Users.ToList() : context.Users.Where(filter).ToList() ;
-            }
-        }
-
-        public void Update(int id, User entity)
-        {
-            using (EatItContext context = new EatItContext())
-            {
-                var e = context.Users.FirstOrDefault(x => x.UserId == id);
-                if (entity != null && id==entity.UserId)
-                {
-                    e.UserName = entity.UserName;
-                    context.Users.Update(e);
-                    context.SaveChanges() ;
-                }
-            }
-        }
+        
     }
 }
