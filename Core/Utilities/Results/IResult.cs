40806d48e15a5920cf13845da3c4fa9d52234539
Özge Odabaş <180315051@ogr.cbu.edu.tr>
2023-12-04 22:35:34 +0300

    results structure created

---
+++ Core/Utilities/Results/IResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    //for voids
+    public interface IResult
+    {
+        string Message { get; }
+        bool IsSuccess { get; }
+
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public string Message { get; }
+
+        public bool IsSuccess { get; }
+
+        public Result(bool success, string message):this(success)
+        {
+           Message = message;
+        }
+
+        public Result(bool success)
+        {
+           IsSuccess = success;
+        }
+    }
+}
