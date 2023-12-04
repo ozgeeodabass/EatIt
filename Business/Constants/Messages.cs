ab6da809035f6e09b73d1b68331c7e33c6f401a0
Özge Odabaş <180315051@ogr.cbu.edu.tr>
2023-12-04 22:36:13 +0300

    messages for successfull or failed operations

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string added = "Successfully Added";
+        public static string deleted = "Successfully Deleted";
+        public static string updated = "Successfully Updated";
+        public static string failed = "Operation Failed";
+        public static string success = "Operation Completed Successfully";
+    }
+}
