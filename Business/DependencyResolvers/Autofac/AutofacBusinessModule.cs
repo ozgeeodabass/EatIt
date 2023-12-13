e51c6da1968f1c22b07cc54f43bd2529b95d29bb
Özge Odabaş <180315051@ogr.cbu.edu.tr>
2023-12-14 02:36:08 +0300

    switch to autofac from .net ioc container

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //singleton lifetime
+            builder.RegisterType<RecipeManager>().As<IRecipeService>().SingleInstance();
+            builder.RegisterType<EfRecipeDal>().As<IRecipeDal>().SingleInstance();
+
+            builder.RegisterType<IngredientManager>().As<IIngredientService>().SingleInstance();
+            builder.RegisterType<EfIngredientDal>().As<IIngredientDal>().SingleInstance();
+
+            builder.RegisterType<CuisineManager>().As<ICuisineService>().SingleInstance();
+            builder.RegisterType<EfCuisineDal>().As<ICuisineDal>().SingleInstance();
+
+            builder.RegisterType<UserManager>().As<IUserService>().SingleInstance();
+            builder.RegisterType<EfUserDal>().As<IUserDal>().SingleInstance();
+        }
+    }
+}
--- EatItWebApi/Program.cs
+++ EatItWebApi/Program.cs
@@ -1,8 +1,12 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using Core.Utilities;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,17 +18,24 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<IUserService, UserManager>();
-builder.Services.AddSingleton<IUserDal, EfUserDal>();
+//builder.Services.AddSingleton<IUserService, UserManager>();
+//builder.Services.AddSingleton<IUserDal, EfUserDal>();
 
-builder.Services.AddSingleton<ICuisineService, CuisineManager>();
-builder.Services.AddSingleton<ICuisineDal, EfCuisineDal>();
+//builder.Services.AddSingleton<ICuisineService, CuisineManager>();
+//builder.Services.AddSingleton<ICuisineDal, EfCuisineDal>();
 
-builder.Services.AddSingleton<IIngredientService, IngredientManager>();
-builder.Services.AddSingleton<IIngredientDal, EfIngredientDal>();
 
-builder.Services.AddSingleton<IRecipeService, RecipeManager>();
-builder.Services.AddSingleton<IRecipeDal, EfRecipeDal>();
+//builder.Services.AddSingleton<IIngredientService, IngredientManager>();
+//builder.Services.AddSingleton<IIngredientDal, EfIngredientDal>();
+
+//builder.Services.AddSingleton<IRecipeService, RecipeManager>();
+//builder.Services.AddSingleton<IRecipeDal, EfRecipeDal>();
+
+//Autofac
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory()).ConfigureContainer<ContainerBuilder>(builder=>
+{
+    builder.RegisterModule(new AutofacBusinessModule());
+});
 
 
 builder.Services.AddDbContext<EatItContext>();
