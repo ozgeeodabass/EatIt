f84720f77cff46f2c12945c1d6c28e835edcdaae
Özge Odabaş <180315051@ogr.cbu.edu.tr>
2023-12-18 20:18:35 +0300

    static validationtool class writed in core layer

---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+    public static class ValidationTool
+    {
+        public static void Validate(IValidator validater, object entity)
+        {
+            var context = new ValidationContext<object>(entity);
+            var result = validater.Validate(context);
+            if (!result.IsValid)
+                throw new ValidationException(result.Errors);
+        }
+    }
+}
