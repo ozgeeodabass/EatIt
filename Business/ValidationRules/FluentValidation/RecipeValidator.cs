198e3ea8ad27175f2e57b4c37a816cd7faa74a2a
Özge Odabaş <180315051@ogr.cbu.edu.tr>
2023-12-18 19:23:06 +0300

    fluent validation package added and validator classes created

---
+++ Business/ValidationRules/FluentValidation/RecipeValidator.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class RecipeValidator : AbstractValidator<Recipe>
+    {
+        public RecipeValidator()
+        {
+            RuleFor(p => p.Ingredients).NotEmpty();
+            RuleFor(p => p.RecipeName).NotEmpty();
+            RuleFor(p => p.RecipeDescription).NotEmpty();
+            RuleFor(p => p.CookingTime).NotEmpty();
+            RuleFor(p => p.PreparationTime).NotEmpty();
+            RuleFor(p => p.UserId).NotEmpty();
+        }
+    }
+}
