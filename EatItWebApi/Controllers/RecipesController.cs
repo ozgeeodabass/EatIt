6b08a0686597088a9946f9354aba6efc39fea9d8
Özge Odabaş <180315051@ogr.cbu.edu.tr>
2023-12-20 00:10:08 +0300

    recipe controller route removed

--- EatItWebApi/Controllers/RecipesController.cs
+++ EatItWebApi/Controllers/RecipesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EatItWebApi.Controllers
 {
-    [Route("api/[controller]/[action]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class RecipesController : ControllerBase
     {
